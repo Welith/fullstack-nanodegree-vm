d18bcc7d0d892d5c3f216b9dc71967cf18be585e
Boris Kolev <boris.kolev@orbisagency.com>
2019-08-05 21:50:19 +0300

    CRUD for restaurant

--- vagrant/menu_builder/webserver.py
+++ vagrant/menu_builder/webserver.py
@@ -1,16 +1,41 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import cgi
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
 
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind=engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
 
 class webserverHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
 		try:
 			if self.path.endswith("/hello"):
-				self:send_response(200)
+				self.send_response(200)
+				self.send_header('Content-type', 'text/html')
+				self.end_headers()
+				printer = ""
+				allRestaurants = session.query(Restaurant).all()
+				for restaurant in allRestaurants:
+					printer += restaurant.name
+					printer += "<br>"
+					
+					
+				output = ""
+				output += "<html><body> %s" % printer
+				output += "</body></html>"
+				self.wfile.write(output)
+				return
+			
+			if self.path.endswith("/hola"):
+				self.send_response(200)
 				self.send_header('Content-type', 'text/html')
 				self.end_headers()
 
 				output = ""
-				output += "<html><body>Hello!</body></html>"
+				output += "<html><body>Hola!</body></html>"
 				self.wfile.write(output)
 				return
 
@@ -18,18 +43,43 @@ class webserverHandler(BaseHTTPRequestHandler):
 			self.send_error(404, "File not found %s" % self.path)
 
 
+	def do_POST(self):
+		try:
+			self.send_response(301)
+			self.end_headers()
+
+			ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+			if ctype == 'multipart/form-data':
+				fields = cgi.parse_multipart(self.rfile, pdict)
+				messagecontent = fields.get('message')
+
+			output = ""
+			output += "<html><body>"
+			output += "<h2> Okay, how about this: </h2>"
+			output += "<h1> %s </h1>" % messagecontent[0]
+
+			output += "<form method='POST' enctype='multipart/form-data' action'/hello'><h2>What would you like me to say?" \
+			"</h2><input name='message' type='text'><input type='submit' value='Submit'></form>"
+			output += "</body></html>"
+			self.wfile.write(output)
+			print(output)
+
+
+		except:
+			pass
+
 def main():
 	try:
 		port = 8080
 		server = HTTPServer(('',port), webserverHandler)
-		print "Web server running on port %s" % port
+		print("Web server running on port %s" % port)
 		server.serve_forever()
-
-
 	except KeyboardInterrupt:
-		print("ˆC entered, server is stopping...")
+		print("C entered, server is stopping...")
 		server.socket.close()
 
 
-if __name__ == 'main':
+
+
+if __name__ == '__main__':
 	main()
